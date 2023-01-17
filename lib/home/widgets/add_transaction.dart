615fcadaaab99ff407afb27bea92691bcdd33e98
sufiyan <sufiyansakkeer616@gmail.com>
2023-01-17 20:19:50 +0530

    add transaction validation added

--- lib/home/widgets/add_transaction.dart
+++ lib/home/widgets/add_transaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:money_track/categories/category_app_popup.dart';
 
 import 'package:money_track/db/category/db_category.dart';
 import 'package:money_track/db/transaction/db_transaction_function.dart';
@@ -22,6 +23,9 @@ class _AddTransactionState extends State<AddTransaction> {
   CategoryType? _selectedCategoryType;
   CategoryModel? _selectedCategoryModel;
   String? _categoryId;
+  final _formKey = GlobalKey<FormState>();
+  String _categoryItemValidationText = '';
+  String _dateValidationText = '';
 
   @override
   void initState() {
@@ -39,172 +43,275 @@ class _AddTransactionState extends State<AddTransaction> {
           'Add Transaction',
         ),
       ),
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _amountTextEditingController,
-                // validator: (value) {
-                //   if (value == null || value.isEmpty) {
-                //     return 'Enter Amount';
-                //   }
-                //   return null;
-                // },
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: 'Amount',
-                ),
-              ),
-              TextFormField(
-                controller: _notesTextEditingController,
-                decoration: const InputDecoration(
-                  hintText: 'Notes',
-                ),
-              ),
-              //date of the transaction
-              TextButton.icon(
-                onPressed: (() async {
-                  final selectedTempDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime.now().subtract(
-                      const Duration(
-                        days: 30,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                selectCategoryType(),
+                //category drop down button and add category button
+                selectCategoryItem(context),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      _categoryItemValidationText,
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 192, 29, 17),
+                        fontSize: 12,
                       ),
                     ),
-                    lastDate: DateTime.now(),
-                  );
-                  if (selectedTempDate == null) {
-                    return;
-                  } else {
-                    print(selectedTempDate);
-                    setState(() {
-                      _selectedDateTime = selectedTempDate;
-                    });
-                  }
-                }),
-                icon: const Icon(
-                  Icons.calendar_today,
+                  ],
                 ),
-                label: Text(
-                  _selectedDateTime == null
-                      ? 'Select Your Date'
-                      : parseDateTime(_selectedDateTime!),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    children: [
-                      // RadioButton('Income', CategoryType.income),
-                      Radio(
-                        // value is what the radio button is holding
-                        value: CategoryType.income,
-                        //user selected value is group value ,
-                        groupValue: _selectedCategoryType,
-                        onChanged: ((value) {
-                          setState(() {
-                            _selectedCategoryType = CategoryType.income;
-                            _categoryId = null;
-                          });
-                        }),
-                      ),
-                      const Text(
-                        'Income',
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      // RadioButton('Expense', CategoryType.expense),
-                      Radio(
-                        value: CategoryType.expense,
-                        groupValue: _selectedCategoryType,
-                        onChanged: ((value) {
-                          setState(() {
-                            _selectedCategoryType = CategoryType.expense;
-                            _categoryId = null;
-                          });
-                        }),
-                      ),
-                      const Text(
-                        'Expense',
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              //category drop down button
-              DropdownButton(
-                hint: const Text('select category'),
-                value: _categoryId,
-                items: (_selectedCategoryType == CategoryType.income
-                        ? CategoryDb.instance.incomeCategoryListListener
-                        : CategoryDb.instance.expenseCategoryListListener)
-                    .value
-                    .map((e) {
-                  return DropdownMenuItem(
-                    value: e.id,
-                    child: Text(
-                      e.categoryName,
+                TextFormField(
+                  controller: _amountTextEditingController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter Amount';
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: 'Amount',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                      ), //<-- SEE HERE
                     ),
-                    onTap: () {
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  controller: _notesTextEditingController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter the purpose';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Notes',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                      ), //<-- SEE HERE
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  minLines: 5, // <-- SEE HERE
+                  maxLines: 5,
+                ),
+                //date of the transaction
+                TextButton.icon(
+                  onPressed: (() async {
+                    final selectedTempDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now().subtract(
+                        const Duration(
+                          days: 30,
+                        ),
+                      ),
+                      lastDate: DateTime.now(),
+                    );
+                    if (selectedTempDate == null) {
+                      return;
+                    } else {
                       setState(() {
-                        _selectedCategoryModel = e;
+                        _selectedDateTime = selectedTempDate;
                       });
-                    },
-                  );
-                }).toList(),
-                onChanged: ((selectedValue) {
-                  // print(selectedValue);
-                  setState(() {
-                    _categoryId = selectedValue;
-                  });
-                }),
-              ),
-              // DropdownButton(
-              //   hint: const Text('Select Category'),
-              //   items: CategoryDb.instance.incomeCategoryListListener.value
-              //       .map((e) {
-              //     return DropdownMenuItem(
-              //       value: e.id,
-              //       child: Text(e.categoryName),
-              //     );
-              //   }).toList(),
-              //   onChanged: ((selectedValue) {
-              //     print(selectedValue);
-              //   }),
-              // ),
-
-              // submit button
-              ElevatedButton(
-                onPressed: (() {
-                  addTransaction();
-
-                  // if (_formKey.currentState!.validate()) {
-                  //   // If the form is valid, display a snackbar. In the real world,
-                  //   // you'd often call a server or save the information in a database.
-                  //   ScaffoldMessenger.of(context).showSnackBar(
-                  //     const SnackBar(
-                  //       content: Text(
-                  //         'Category Added',
-                  //       ),
-                  //       backgroundColor: Color(0xFF2E49FB),
-                  //     ),
-                  //   );
-                  // }
-                }),
-                child: const Text(
-                  'Submit',
+                    }
+                  }),
+                  icon: const Icon(
+                    Icons.calendar_today,
+                  ),
+                  label: Text(
+                    _selectedDateTime == null
+                        ? 'Select Your Date'
+                        : parseDateTime(_selectedDateTime!),
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  _dateValidationText,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 192, 29, 17),
+                    fontSize: 12,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                // submit button
+                ElevatedButton(
+                  onPressed: (() {
+                    if (_selectedDateTime == null) {
+                      setState(() {
+                        _dateValidationText = 'please Select Date';
+                      });
+                    } else {
+                      setState(() {
+                        _dateValidationText = '';
+                      });
+                    }
+                    if (_categoryId == null) {
+                      setState(() {
+                        _categoryItemValidationText = 'Please Select Category';
+                      });
+                    } else {
+                      _categoryItemValidationText = '';
+                    }
+
+                    if (_formKey.currentState!.validate()) {}
+                    addTransaction();
+                  }),
+                  child: const Text(
+                    'Submit',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Row selectCategoryItem(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(
+              // color: Colors.black38,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 5, top: 5, bottom: 5),
+            child: DropdownButton(
+              dropdownColor: Colors.amber,
+              elevation: 0,
+
+              // border: Border.all(color: Colors.redAccent, width: 2),
+              hint: const Text('select category'),
+              value: _categoryId,
+              items: (_selectedCategoryType == CategoryType.income
+                      ? CategoryDb.instance.incomeCategoryListListener
+                      : CategoryDb.instance.expenseCategoryListListener)
+                  .value
+                  .map((e) {
+                return DropdownMenuItem(
+                  value: e.id,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        // color: Colors.black38,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    width: 270,
+                    child: Text(
+                      e.categoryName,
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _selectedCategoryModel = e;
+                    });
+                  },
+                );
+              }).toList(),
+              onChanged: ((selectedValue) {
+                // print(selectedValue);
+                setState(() {
+                  _categoryId = selectedValue;
+                });
+              }),
+            ),
+          ),
+        ),
+        IconButton(
+          onPressed: (() {
+            showCategoryAddPopup(context);
+          }),
+          icon: const Icon(
+            Icons.add_circle_outline,
+            color: Color(0xFF2E49FB),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row selectCategoryType() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          children: [
+            //   OutlineButton(
+            //   onPressed: () {
+            //     setState(() {
+            //       value = index;
+            //     });
+            //   },
+            //   child: Text(
+            //     text,
+            //     style: TextStyle(
+            //       color: (value == index) ? Colors.green : Colors.black,
+            //     ),
+            //   ),
+            //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            //   borderSide:
+            //       BorderSide(color: (value == index) ? Colors.green : Colors.black),
+            // );// RadioButton('Income', CategoryType.income),
+            Radio(
+              // value is what the radio button is holding
+              value: CategoryType.income,
+              //user selected value is group value ,
+              groupValue: _selectedCategoryType,
+              onChanged: ((value) {
+                setState(() {
+                  _selectedCategoryType = CategoryType.income;
+                  _categoryId = null;
+                });
+              }),
+            ),
+            const Text(
+              'Income',
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            // RadioButton('Expense', CategoryType.expense),
+            Radio(
+              value: CategoryType.expense,
+              groupValue: _selectedCategoryType,
+              onChanged: ((value) {
+                setState(() {
+                  _selectedCategoryType = CategoryType.expense;
+                  _categoryId = null;
+                });
+              }),
+            ),
+            const Text(
+              'Expense',
+            ),
+          ],
+        ),
+      ],
     );
   }
 
