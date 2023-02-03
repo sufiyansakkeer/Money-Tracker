6cd551c39c3bed4b5cbbe45b0df7c1412073c134
sufiyan <sufiyansakkeer616@gmail.com>
2023-02-03 15:51:21 +0530

    converted into app bundle

--- lib/navigation_drawer/navigation_drawer.dart
+++ lib/navigation_drawer/navigation_drawer.dart
@@ -177,7 +177,7 @@ class NavigationDrawerClass extends StatelessWidget {
               child: ListTile(
                 onTap: () {
                   Share.share(
-                      'hey! check out this new app https://play.google.com/store/search?q=money%20tracker&c=apps');
+                      'hey! check out this new app https://play.google.com/store/search?q=money%20track&c=apps');
                 },
                 title: const Text(
                   'Share',
